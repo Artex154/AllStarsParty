9f6f6bb7d96455f5fd68bde3ba11030a1cbe33d8
Artex154 <a.stockelynck12@outlook.com>
2025-12-21 01:32:25 +0100

    Added base gamerules

--- src/main/java/be/artex/allStarsParty/AllStarsParty.java
+++ src/main/java/be/artex/allStarsParty/AllStarsParty.java
@@ -1,6 +1,8 @@
 package be.artex.allStarsParty;
 
 import be.artex.allStarsParty.listener.ConnectionsEventListener;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AllStarsParty extends JavaPlugin {
@@ -13,6 +15,12 @@ public final class AllStarsParty extends JavaPlugin {
         instance = this;
         inGame = false;
         maxPlayers = 4;
+
+        World world = Bukkit.getWorlds().get(0);
+
+        world.setGameRuleValue("naturalRegeneration", "false");
+        world.setGameRuleValue("doDaylightCycle", "false");
+        world.setGameRuleValue("doMobSpawning", "false");
 
         getServer().getPluginManager().registerEvents(new ConnectionsEventListener(), this);
     }
