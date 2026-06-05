09599db0ad3b7da3adc925bef3b334e2d60ba00c
Artex154 <a.stockelynck12@outlook.com>
2026-06-05 20:19:04 +0200

    Added Muzan

---
+++ src/main/java/be/artex/allStarsParty/role/DS/Muzan.java
@@ -0,0 +1,40 @@
+package be.artex.allStarsParty.role.DS;
+
+import be.artex.allStarsParty.TextUtil;
+import be.artex.allStarsParty.logic.Role;
+import be.artex.allStarsParty.logic.Side;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+public class Muzan extends Role {
+    private static final String DESCRIPTION = TextUtil.BORDER +
+            "\n" + ChatColor.WHITE + " Vous êtes " + ChatColor.RED + "Muzan" + ChatColor.WHITE + "." +
+            "\n Vous possédez 105% de " + ChatColor.RED + "force" + ChatColor.WHITE + "." +
+            "\n Quand vous " + ChatColor.RED + "tappez " + ChatColor.WHITE + "un joueur, vous gagnez " + ChatColor.LIGHT_PURPLE + "10% des dégats infligés " + ChatColor.WHITE + "." +
+            "\n" + TextUtil.BORDER;
+
+    @Override
+    public String getName() {
+        return "Muzan";
+    }
+
+    @Override
+    public Side getSide() {
+        return Side.DS;
+    }
+
+    @Override
+    public String getDescription() {
+        return DESCRIPTION;
+    }
+
+    @Override
+    public int getStrength() {
+        return 105;
+    }
+
+    @Override
+    public void onHit(Player player, Player damager, double damage) {
+        damager.setHealth(damager.getHealth() + (damage / 10));
+    }
+}
