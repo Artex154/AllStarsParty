1019d265b14174bd1b2f7dd74d42f44e69c2b144
Artex154 <a.stockelynck12@outlook.com>
2026-06-26 04:44:10 +0200

    Added Fire resistance to Shoto

--- src/main/java/be/artex/allStarsParty/role/rewrited/shoto/Shoto.java
+++ src/main/java/be/artex/allStarsParty/role/rewrited/shoto/Shoto.java
@@ -4,6 +4,9 @@ import be.artex.allStarsParty.api.items.ASPItem;
 import be.artex.allStarsParty.api.Role;
 import be.artex.allStarsParty.api.Side;
 import be.artex.allStarsParty.registry.ItemRegistry;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,5 +36,10 @@ public class Shoto extends Role {
     @Override
     public int getResistance() {
         return 110;
+    }
+
+    @Override
+    public void whenAssigned(Player player) {
+        player.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, Integer.MAX_VALUE, 0));
     }
 }
