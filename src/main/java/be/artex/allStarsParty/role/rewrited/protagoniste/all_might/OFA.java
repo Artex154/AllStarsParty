e70209aeb293238a32f62f69a342caff8b2ea6ff
Artex154 <a.stockelynck12@outlook.com>
2026-06-30 23:46:26 +0200

    slight buff for All Might

--- src/main/java/be/artex/allStarsParty/role/rewrited/protagoniste/all_might/OFA.java
+++ src/main/java/be/artex/allStarsParty/role/rewrited/protagoniste/all_might/OFA.java
@@ -41,7 +41,7 @@ public class OFA extends ASPItem {
             player.sendMessage(Message.info("Vous avez" + ChatColor.RED + " désactivé" + ChatColor.WHITE + " le " + ChatColor.GOLD + ChatColor.BOLD + "One For All" + ChatColor.WHITE + "."));
 
             Strength.removeStrengthFromPlayer(player, 10);
-            Speed.removeSpeedFromPlayer(player, 10);
+            Speed.removeSpeedFromPlayer(player, 15);
             Resistance.removeResistanceFromPlayer(player, 10);
 
             long elapsedMillis = currentSystemMillis - playersWhenActivated.get(player);
@@ -63,13 +63,13 @@ public class OFA extends ASPItem {
             playersWhenActivated.put(player, currentSystemMillis);
 
             Strength.addStrengthToPlayer(player, 10);
-            Speed.addSpeedToPlayer(player, 10);
+            Speed.addSpeedToPlayer(player, 15);
             Resistance.addResistanceToPlayer(player, 10);
 
             Bukkit.getScheduler().runTaskLater(AllStarsParty.instance, () -> {
                 if (playersWhenActivated.get(player) == currentSystemMillis) {
                     Strength.removeStrengthFromPlayer(player, 10);
-                    Speed.removeSpeedFromPlayer(player, 10);
+                    Speed.removeSpeedFromPlayer(player, 15);
                     Resistance.removeResistanceFromPlayer(player, 10);
 
                     player.sendMessage(Message.warn("Le " + ChatColor.GOLD + ChatColor.BOLD + "One For All" + ChatColor.WHITE + " cesse."));
