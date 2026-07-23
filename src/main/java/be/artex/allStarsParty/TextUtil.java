08b42fa6aa267f3fc48a3ea15450a7edf18207b3
RaftDev <theraft08@gmail.com>
2026-07-24 00:26:01 +0200

    Removed old AllStarts (Rewrite on a rewrite)

--- src/main/java/be/artex/allStarsParty/TextUtil.java
+++
@@ -1,7 +0,0 @@
-package be.artex.allStarsParty;
-
-import org.bukkit.ChatColor;
-
-public class TextUtil {
-    public static final String BORDER = ChatColor.DARK_GRAY + "" + ChatColor.STRIKETHROUGH + "                                                                                ";
-}
--- src/main/java/be/artex/allStarsParty/role/rewrited/duo/jigoZen/zenitsu/SleepState.java
+++
@@ -1,7 +0,0 @@
-package be.artex.allStarsParty.role.rewrited.duo.jigoZen.zenitsu;
-
-public enum SleepState {
-    AWAKE,
-    ASLEEP_TEMPORARY,
-    ASLEEP_PERMANENT
-}
